6b4f952579c25130080a6b3bda211b99529da5e2
timu-c <56608281+timu-c@users.noreply.github.com>
2019-11-18 12:46:42 -0800

    Create lesson1.py
    This is for Christine's LnL class

---
+++ lesson1.py
@@ -0,0 +1,11 @@
+# Variable being assigned a string value
+subject = "HallmarkLabs' Lunch and Learn!"
+
+# Ask the user for their name
+user_name = input("What is your name? ") 
+
+# Putting together string and a variable value together
+welcome_message = "Welcome " + user_name + ' to ' + subject
+
+# Display message to screen
+print(welcome_message)
